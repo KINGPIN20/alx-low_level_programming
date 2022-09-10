f6029082409a8ee20933ad1da30abc36d4ae4928
KINGPIN20 <yusufadekanmbi@yahoo.com>
2022-09-10 00:20:08 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+
+/*C program that prints exactly "Programming is like building a multilingual puzzle*/
+int main(void) 
+{
+puts("\" Programming is like building a multilingual puzzle")
+return (0);
+}
+
