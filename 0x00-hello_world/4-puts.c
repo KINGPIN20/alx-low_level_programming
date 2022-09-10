61a9a457c89415d40d3108d336450aa21e23c2fa
KINGPIN20 <yusufadekanmbi@yahoo.com>
2022-09-10 01:17:05 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #include <stdio.h>
-/* Prints Phrase Programming is like building a multilingual puzzle */
-int main(void)
+
+/* Prints Programming is like building a multilingual puzzle */
+
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
