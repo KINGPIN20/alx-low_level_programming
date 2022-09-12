b6af11c79d7ad1b631a60fd13110b4b9be7d8dad
KINGPIN20 <yusufadekanmbi@yahoo.com>
2022-09-12 04:04:23 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,16 +4,15 @@
 
 /**
  * main - prints last digit
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int 1;
+	int n, i;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d\n", n);
 	1 = n % 10;
 	if (1 > 5)
 	{
