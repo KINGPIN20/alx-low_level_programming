f1cb99b6c8edd9ab97ea9f6b95521c3a4f75cfc4
KINGPIN20 <yusufadekanmbi@yahoo.com>
2022-09-12 02:56:21 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,12 +4,12 @@
  * main - prints single digit numbers < 10
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
 	int i;
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
