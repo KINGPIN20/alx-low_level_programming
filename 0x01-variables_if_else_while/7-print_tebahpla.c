24b7f6f99cab525c87154c50efe56c42a584c185
KINGPIN20 <yusufadekanmbi@yahoo.com>
2022-09-12 03:08:13 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 26; i > 0; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
