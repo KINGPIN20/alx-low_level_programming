cc72b0940e205880f0bff8edcfbfdabc57363c10
KINGPIN20 <yusufadekanmbi@yahoo.com>
2022-09-14 07:52:43 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+/**
+*prints the alphabet, in lowercase, followed by a new line
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	
+	for (i == 97; i <= 122; i++)
+	{
+		putchar (i);
+		putchar ('\n');
+	}
+	
+	return (0);
+			
+}
