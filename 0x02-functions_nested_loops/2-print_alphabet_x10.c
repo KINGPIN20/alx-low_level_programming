a471eb6dea0149a23061c6751d5724daf32f4973
KINGPIN20 <yusufadekanmbi@yahoo.com>
2022-09-14 15:54:38 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,3 +4,17 @@
 * print_alphabet_x10 - prints alphabets 10 times
 *
 * Return: 1 Always success
+*/
+
+void print_alphabet_x10(void)
+{
+int c = 0;
+char a;
+for (c = 0; c <= 9; c++)
+{
+for (a = 'a'; a <= 'z'; a++)
+
+_putchar(a);
+_putchar('\n');
+}
+}
